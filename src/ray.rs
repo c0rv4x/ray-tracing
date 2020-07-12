2fe05db7dbf358e7b7552cb165511ce1d1bd9ea4
c0rvax <c0r74x@gmail.com>
2020-07-13 00:41:03 +0300

    Ray is now typed tuple

--- src/ray.rs
+++ src/ray.rs
@@ -1,22 +1,32 @@
-mod vec3;
+use crate::vec3;
 use vec3::Vec3;
 
 pub struct Ray {
-    A: Vec3,
-    B: Vec3,
+    origin: Vec3,
+    direction: Vec3,
 }
 
 impl Ray {
-    pub fn new(A: Vec3, B: Vec3) -> Vec3 {
-        Ray(A, B)
+    pub fn new(a: Vec3, b: Vec3) -> Ray {
+        Ray {
+            origin: a,
+            direction: b,
+        }
     }
-    pub fn origin() -> Vec3 {
-        A
+    pub fn origin(&self) -> Vec3 {
+        self.origin
     }
-    pub fn direction() -> Vec3 {
-        B
+    pub fn direction(&self) -> Vec3 {
+        self.direction
     }
-    pub fn point_at_parameter(t: float) -> Vec3 {
-        self.A + self.B * t
+    pub fn point_at_parameter(&self, t: f32) -> Vec3 {
+        self.origin + self.direction * t
     }
+}
+
+pub fn color(r: Ray) -> Vec3 {
+    let direction: Vec3 = r.direction().unit_vector();
+    let t: f32 = 0.5 * (direction.y() + 1.0);
+    // println!("{}", t);
+    (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
 }
