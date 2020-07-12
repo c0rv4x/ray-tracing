78e953853f59542c64121d78e8a0800cd3e7182f
c0rvax <c0r74x@gmail.com>
2020-07-13 00:51:10 +0300

    Fix coloring normalization (259 not 256)

--- src/main.rs
+++ src/main.rs
@@ -23,11 +23,11 @@ fn main() {
             let direction_ray: Ray = Ray::new(origin, left_corner + u * horizontal + v * vertical);
 
             let color_multipliers: Vec3 = color(direction_ray);
-            let red: usize = (259.99 * color_multipliers.r()) as usize;
-            let green: usize = (259.99 * color_multipliers.g()) as usize;
-            let blue: usize = (259.99 * color_multipliers.b()) as usize;
-            // println!("{} {} {}", red, green, blue);
-            color_multipliers.print();
+            let red: usize = (255.99 * color_multipliers.r()) as usize;
+            let green: usize = (255.99 * color_multipliers.g()) as usize;
+            let blue: usize = (255.99 * color_multipliers.b()) as usize;
+            println!("{} {} {}", red, green, blue);
+            // color_multipliers.print();
         }
     }
 }
