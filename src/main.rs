b5f560b1e4fb3cc0bee1b952ace906f104a9695a
c0rvax <c0r74x@gmail.com>
2020-07-13 00:41:30 +0300

    Working towards ray implementation

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,33 @@
-// fn format_pixel(r: u32, g: u32, b: u32) -> u32 {
-//     return r + g + b;
-// }
-
+mod ray;
 mod vec3;
+use ray::color;
+use ray::Ray;
 use vec3::Vec3;
 
 fn main() {
-    let xsize: u32 = 100;
+    let xsize: u32 = 200;
     let ysize: u32 = 100;
 
     println!("P3");
     println!("{} {}", xsize, ysize);
     println!("255");
+
+    let left_corner: Vec3 = Vec3::new(-2.0, -1.0, -1.0);
+    let origin: Vec3 = Vec3::new(0.0, 0.0, 0.0);
+    let horizontal: Vec3 = Vec3::new(4.0, 0.0, 0.0);
+    let vertical: Vec3 = Vec3::new(0.0, 2.0, 0.0);
     for j in (0..ysize).rev() {
         for i in 0..xsize {
-            let r: f32 = i as f32 / xsize as f32;
-            let g: f32 = j as f32 / ysize as f32;
-            let b: f32 = 0.2;
-            let col = Vec3::new(r, g, b);
+            let u: f32 = i as f32 / xsize as f32;
+            let v: f32 = j as f32 / ysize as f32;
+            let direction_ray: Ray = Ray::new(origin, left_corner + u * horizontal + v * vertical);
 
-            let r: i32 = (256 as f32 * col.r()) as i32;
-            let g: i32 = (256 as f32 * col.g()) as i32;
-            let b: i32 = (256 as f32 * col.b()) as i32;
-            println!("{} {} {}", r, g, b);
+            let color_multipliers: Vec3 = color(direction_ray);
+            let red: usize = (259.99 * color_multipliers.r()) as usize;
+            let green: usize = (259.99 * color_multipliers.g()) as usize;
+            let blue: usize = (259.99 * color_multipliers.b()) as usize;
+            // println!("{} {} {}", red, green, blue);
+            color_multipliers.print();
         }
     }
-    (3.0 * Vec3::new(1.0, 2.4, 4.4)).print();
-    (Vec3::new(1.0, 2.4, 4.4) * 3.0).print()
 }
